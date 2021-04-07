23e9c6d82ee5722ab27e5f59d299d86e68614867
Callum Hutchinson <callumhutchie@hotmail.co.uk>
2021-04-07 23:33:34 +0100

    back to particles

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   new SweetScroll({/* some options */});
 
-  /* tsParticles.loadJSON(@dom-id, @path-json); */
-  tsParticles.load('tsparticles', {
-    "fpsLimit": 60,
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS('particles-js', {
     "particles": {
       "number": {
         "value": 30,
         "density": {
           "enable": true,
-          "area": 800
+          "value_area": 800
         }
       },
       "color": {
@@ -18,31 +17,40 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       "shape": {
         "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
         "polygon": {
-          "sides": 5
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
         }
       },
       "opacity": {
         "value": 0.5,
         "random": false,
-        "animation": {
+        "anim": {
           "enable": false,
           "speed": 1,
-          "minimumValue": 0.1,
+          "opacity_min": 0.1,
           "sync": false
         }
       },
       "size": {
         "value": 3,
         "random": true,
-        "animation": {
+        "anim": {
           "enable": false,
           "speed": 19.18081918081918,
-          "minimumValue": 0.1,
+          "size_min": 0.1,
           "sync": false
         }
       },
-      "links": {
+      "line_linked": {
         "enable": true,
         "distance": 150,
         "color": "#ffffff",
@@ -55,22 +63,24 @@ document.addEventListener("DOMContentLoaded", function () {
         "direction": "none",
         "random": true,
         "straight": false,
-        "outMode": "out",
+        "out_mode": "out",
+        "bounce": false,
         "attract": {
           "enable": false,
           "rotateX": 600,
           "rotateY": 1200
         }
-      }
+      },
+      nb: 80
     },
     "interactivity": {
-      "detectsOn": "canvas",
+      "detect_on": "canvas",
       "events": {
-        "onHover": {
+        "onhover": {
           "enable": false,
           "mode": "grab"
         },
-        "onClick": {
+        "onclick": {
           "enable": true,
           "mode": "push"
         },
@@ -79,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "modes": {
         "grab": {
           "distance": 400,
-          "links": {
+          "line_linked": {
             "opacity": 1
           }
         },
@@ -87,21 +97,22 @@ document.addEventListener("DOMContentLoaded", function () {
           "distance": 400,
           "size": 40,
           "duration": 2,
-          "opacity": 8
+          "opacity": 8,
+          "speed": 3
         },
         "repulse": {
           "distance": 200,
           "duration": 0.4
         },
         "push": {
-          "quantity": 4
+          "particles_nb": 4
         },
         "remove": {
-          "quantity": 2
+          "particles_nb": 2
         }
       }
     },
-    "detectRetina": true
+    "retina_detect": true
   });
 
 }, false);
